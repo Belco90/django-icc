f3fec80a4c786cbe8bb4613630f373be765d45c7
Mario Beltrán Alarcón <belco90@gmail.com>
2017-02-28 20:50:18 +0100

    Create django project

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Django ICC
+Django ICC
+==========
+
 Django application to provide an internal communication channel for django projects through a tickets system
+
+Quick start
+-----------
+
+1. Add "django-icc" to your INSTALLED_APPS setting like this:
+
+    
+    INSTALLED_APPS = [
+        ...
+        'django-icc',
+        ...
+    ]
+
+2. Pending
--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-SHORT_DESCRIPTION = 'Django application to provide an internal communication channel for django projects through a ' \
-                    'tickets system'
+SHORT_DESCRIPTION = ('Django application to provide an internal communication channel for django projects through a '
+                     'tickets system')
 
 setup(
     name='django-icc',
@@ -25,12 +25,13 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.10',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
